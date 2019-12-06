df771f164f01bffef04b0286c68d35db62e55b20
GuangJuChen <3546766954@qq.com>
2019-12-07 00:17:54 +0800

    login

--- src/main/java/com/chen/sys/controller/UserController.java
+++ src/main/java/com/chen/sys/controller/UserController.java
@@ -4,6 +4,7 @@ package com.chen.sys.controller;
 import com.chen.sys.entity.User;
 import com.chen.sys.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author chenGJ
  * @since 2019-12-05
  */
-@RestController
+@Controller
 @RequestMapping("/sys/user")
 public class UserController {
 
@@ -32,9 +33,9 @@ public class UserController {
      * @Date: 2019/12/5 22:35
     **/
     @RequestMapping("/add")
-    public User addUser(User user){
+    public String addUser(User user){
         System.out.println(11111);
-        return userService.addUser(user);
+        return "index";
     }
 
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<a href="${pageContext.request.contextPath}/sys/user/add?username=chen" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
 </body>
 </html>
