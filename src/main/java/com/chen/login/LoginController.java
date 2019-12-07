ffdbac87f99da8cf5f65021c869cd961e350a2e2
GuangJuChen <3546766954@qq.com>
2019-12-07 08:44:47 +0800

    login

---
+++ src/main/java/com/chen/login/LoginController.java
@@ -0,0 +1,14 @@
+package com.chen.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/spring")
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login(){
+        return "index";
+    }
+}
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -2,5 +2,6 @@
 <body>
 <h2>Hello World!</h2>
 <a href="${pageContext.request.contextPath}/sys/user/add?username=chen" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+<img src="../../loginBackground.png">
 </body>
 </html>
